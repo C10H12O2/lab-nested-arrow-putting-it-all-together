8071d72d5e9cb46c930093f85411542941b3cf43
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2025-12-02 01:24:19 +0300

    Added the neccessary changes after multiple tests

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+function createLoginTracker(user) {
+  let attemptCount = 0;
+
+  return (passwordAttempt) => {
+    if (attemptCount >= 3) {
+      return 'Account locked due to too many failed login attempts';
+    }
+    if (passwordAttempt === user.password) {
+      attemptCount = 0;
+      return 'Login successful';
+    }
+    attemptCount++;
+    if (attemptCount >= 3) {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+    return `Attempt ${attemptCount}: Login failed`;
+  };
+}
 
 
 
